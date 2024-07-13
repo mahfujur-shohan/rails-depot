d95e18f052bf39b3d3a863612f0d9c51b7b07e89
Mahfujur Rahman <mahfujurrahmancityu@gmail.com>
2024-07-13 14:29:30 +0600

    some seed data added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Product.delete_all
+Product.create!(title: "Docker for Rails Developer", description: "Docker does for DevOps what Rails did for web development—it gives you a new set of superpowers. Gone are “works on my machine” woes and lengthy setup tasks, replaced instead by a simple, consistent, Docker-based development environment that will have your team up and running in seconds. Gain hands-on, real-world experience with a tool that’s rapidly becoming fundamental to software development.", image_url: "dfrd.jpeg", price: 10.50)
+Product.create!(title: "Design and Development Great Web Api's", description: "APIs are transforming the business world at an increasing pace. Gain the essential skills needed to quickly design, build, and deploy quality web APIs that are robust, reliable, and resilient.", image_url: "dabgwa.jpeg", price: 15.60)
+Product.create!(title: "Modern Css with Tailwind", description: "Tailwind CSS is an exciting new CSS framework that allows you to design your site by composing simple utility classes to create complex effects. With Tailwind, you can style your text, move your items on the page,", image_url: "mcwt.jpeg", price: 20.00)
